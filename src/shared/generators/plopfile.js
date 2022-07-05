a734362decf54fa38cc8809a26c872ee45397363
Felipe Borges <lipex360@gmail.com>
2022-07-04 23:02:24 -0300

    plopfile updated

--- src/shared/generators/plopfile.js
+++ src/shared/generators/plopfile.js
@@ -1,21 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const migrationGenerator = require('./migrations');
-const typeORMGenerator = require('./modules/typeorm');
-const mongooseGenerator = require('./modules/mongoose');
-const useCasesGenerator = require('./useCases');
-const seedGenerator = require('./seeds');
-const middlewareGenerator = require('./middlewares');
-const providerGenerator = require('./providers');
-const generator = require('./_generator');
-const startGenerator = require('./start');
 
 module.exports = function (plop) {
   plop.setGenerator('Migration', migrationGenerator);
-  // plop.setGenerator('Module - TypeORM', typeORMGenerator);
-  // plop.setGenerator('Module - Mongoose', mongooseGenerator);
-  // plop.setGenerator('UseCase', useCasesGenerator);
-  // plop.setGenerator('Seed', seedGenerator);
-  // plop.setGenerator('Middleware', middlewareGenerator);
-  // plop.setGenerator('Provider', providerGenerator);
-  // plop.setGenerator('Generator', generator);
-  // plop.setGenerator('Start', startGenerator);
 };
